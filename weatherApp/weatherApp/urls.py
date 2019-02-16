dc4e007caaead9d3e3eecc1b2bfc84409e994ef8
Lily Pham <lilypham@dhcp-v106-206.mobile.uci.edu>
2019-02-16 01:15:27 -0800

    Created the first view

--- weatherApp/weatherApp/urls.py
+++ weatherApp/weatherApp/urls.py
@@ -5,7 +5,7 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatters:  url(r'^$', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
+    url(r'^weather/', include('weather.urls')),
     url(r'^admin/', admin.site.urls),
 ]

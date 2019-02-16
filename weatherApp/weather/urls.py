f76d6153fd1b192558c401ea038c5f944482e580
Lily Pham <lilypham@dhcp-v106-206.mobile.uci.edu>
2019-02-16 14:35:49 -0800

    Created userdatapoint and weatherdata models and migrated to databases

--- weatherApp/weather/urls.py
+++ weatherApp/weather/urls.py
@@ -6,7 +6,7 @@ app_name = "weather"
 urlpatterns = [
     path('', views.index, name='index'),
     # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    # path('<int:question_id>/', views.detail, name='detail'),
+    # # ex: /polls/5/vote/
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- weatherApp/weather/views.py
+++ weatherApp/weather/views.py
@@ -11,7 +11,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
 from django.urls import reverse
 
-from .models import Question, Choice
+from .models import UserDataPoint, WeatherData
 
 
 def index(request):
@@ -25,24 +25,20 @@ def index(request):
     #print(mimetypes.guess_type(static('images/right-arrow.svg'))) 
     return render(request, 'weather/index.html')
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'weather/detail.html', {'question': question})
-
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'weather/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('weather:results', args=(question.id,)))
+# def vote(request, question_id):
+    # question = get_object_or_404(Question, pk=question_id)
+    # try:
+    #     selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    # except (KeyError, Choice.DoesNotExist):
+    #     # Redisplay the question voting form.
+    #     return render(request, 'weather/detail.html', {
+    #         'question': question,
+    #         'error_message': "You didn't select a choice.",
+    #     })
+    # else:
+    #     selected_choice.votes += 1
+    #     selected_choice.save()
+    #     # Always return an HttpResponseRedirect after successfully dealing
+    #     # with POST data. This prevents data from being posted twice if a
+    #     # user hits the Back button.
+    #     return HttpResponseRedirect(reverse('weather:results', args=(question.id,)))

c850fcae6673369a9c4ff0bc573c94d4e373020a
Michael Auld <michaelauld@dhcp-v115-156.mobile.uci.edu>
2019-02-17 01:13:05 -0800

    Added blurb generator for index page

--- weatherApp/weather/views.py
+++ weatherApp/weather/views.py
@@ -15,49 +15,71 @@ from django.urls import reverse
 from .models import UserDataPoint, WeatherData
 
 
-def index(request, user_voted=False):
+def index(request):
     currentWeather = apiCaller.get_current_dict()
+    dailyWeather = apiCaller.get_daily_dict()
+    feel = None #GET INPUT!!!!!!!
 
-    context = {
-        'current_temperature': currentWeather['current_temperature'],
-        'current_wind_speed': currentWeather['current_windSpeed'],
-        'user_voted': user_voted,
-    }
+    blurb = ''
+    hour_min_sec = currentWeather['CURRENT_TIME'].split(' ')[1].split(':')
+    time_in_min = int(hour_min_sec[0])*60 + int(hour_min_sec[1]) #0-1440 minutes
+    if 240 <= time_in_min < 720:
+        blurb += "Good Morning! "
+    elif 720 <= time_in_min < 1020:
+        blurb += "Good Afternoon! "
+    elif 1020 <= time_in_min < 1440:
+        blurb += "Good Evening! "
+    else
+        blurb += "Good Day! "
 
-    return render(request, 'weather/index.html', context)
+    blurb += "It's going to be "
+
+    if feel == 0:
+        blurb += "very cold "
+    elif feel == 1:
+        blurb += "a little chilly "
+    elif feel == 2:
+        blurb += "just how you like it "
+    elif feel == 3:
+        blurb += "a bit warm "
+    elif feel == 4:
+        blurb += "very hot "
+    else:
+        blurb += "magical "
+
+    blurb += "outside today."
+
+    if (dailyWeather['daily_summary']):
+        blurb += " It will be raining today, so bring an umbrella! "
+    if (dailyWeather['daily_precipProbability']):
+        blurb += " It will be sunny out today, so bring your shades!"
+    if (dailyWeather['daily_apparentTemperatureHigh'] > 90 and dailyWeather['daily_humidity'] >= 0.98):
+        blurb += " Feels like a lawless swampland out today, so bring your guns!"
+    if (dailyWeather['daily_apparentTemperatureHigh'] > 90 and dailyWeather['daily_windSpeed'] >= 30):
+        blurb += " Hot and windy! Be careful Icarus!"
+    if (dailyWeather['daily_apparentTemperatureHigh'] == 69 and dailyWeather['daily_humidity'] == 69):
+        blurb += " Hehe."
+
+    return render(request, 'weather/index.html', currentWeather)
 
 def comfortAsk(request):
-    return render(request, 'weather/comfortAsk.html')
-
-def submission(request):
-    try:
-        selected_choice = request.POST['choice']
-    except (KeyError):
-        # Redisplay the ask form.
-        return render(request, 'weather/comfortAsk.html', {
-            'error_message': "You didn't select a choice.",
-        })
-
-    # currentWeather = apiCaller.get_current_dict()
-    # currentWeatherData = WeatherData()
-    # dailyWeather = apiCaller.get_daily_dict()
-    # currentWeatherData.temperature = currentWeather['current_Temperature']
-    # currentWeatherData.apparentTemp = currentWeather['current_apparentTemperature']
-    # currentWeatherData.humidity = currentWeather['current_humidity']
-    # currentWeatherData.precip_prob = currentWeather['current_precipProbability']
-    # currentWeatherData.windSpeed = currentWeather['current_windSpeed']
-    # currentWeatherData.cloudiness = currentWeather['current_summary']
-    # currentWeatherData.time = currentWeather['CURRENT_TIME']
-    # currentWeatherData.sunrise = dailyWeather['daily_sunriseTime']
-    # currentWeatherData.sunsetTime = dailyWeather['daily_sunsetTime']
-    # dataPoint = UserDataPoint()
+    currentWeather = apiCaller.get_current_dict()
+    currentWeatherData = WeatherData()
+    dailyWeather = apiCaller.get_daily_dict()
+    currentWeatherData.temperature = currentWeather['current_Temperature']
+    currentWeatherData.apparentTemp = currentWeather['current_apparentTemperature']
+    currentWeatherData.humidity = currentWeather['current_humidity']
+    currentWeatherData.precip_prob = currentWeather['current_precipProbability']
+    currentWeatherData.windSpeed = currentWeather['current_windSpeed']
+    currentWeatherData.cloudiness = currentWeather['current_summary']
+    currentWeatherData.time = currentWeather['CURRENT_TIME']
+    currentWeatherData.sunrise = dailyWeather['daily_sunriseTime']
+    currentWeatherData.sunsetTime = dailyWeather['daily_sunsetTime']
+    dataPoint = UserDataPoint()
     # dataPoint.feeling = [SOME INPUT]
     # dataPoint.recordedWeather = currentWeatherData
     # dataPoint.save()
-
-    return HttpResponseRedirect(reverse('weather', kwargs={user_voted: True}))
-
-
+    return render(request, 'weather/comfortAsk.html')
 
 # def vote(request, question_id):
     # question = get_object_or_404(Question, pk=question_id)

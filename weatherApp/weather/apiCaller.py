546954446949a3af4f3cb05fcc6091b00039bcd7
Lily Pham <lilypham@dhcp-v106-206.mobile.uci.edu>
2019-02-16 16:57:50 -0800

    Added API files

---
+++ weatherApp/weather/apiCaller.py
@@ -0,0 +1,73 @@
+#apiCaller.py
+import json
+import urllib.parse
+import urllib.request
+from datetime import datetime
+import Coordinate_Request
+
+
+
+API_KEY = '0b314396b7522fbd939166f3ed3e53a4'
+
+BASE_URL ='https://api.darksky.net/forecast/'
+#https://api.darksky.net/forecast/0b314396b7522fbd939166f3ed3e53a4/42.3601, -71.0589
+
+
+def build_search_url(lat: float, lon: float):
+    query_parameters= [('latitude', lat), ('longitude', lon)]
+    return BASE_URL + API_KEY + "/" + str(query_parameters[0][1]) + "," + str(query_parameters[1][1])
+
+def get_result(url:str):
+    response = None
+    try:
+        response = urllib.request.urlopen(url)
+        json_text = response.read().decode(encoding = 'utf-8')
+
+        return json.loads(json_text)
+
+    finally:
+        if response!= None:
+            response.close()
+
+
+def pull_current_data(json_object):
+    new_dict = {}
+    new_dict['current_windSpeed'] = json_object['currently']['windSpeed']
+    new_dict['current_humidity'] = json_object['currently']['humidity']
+    new_dict['current_apparentTemperature'] = json_object['currently']['apparentTemperature']
+    new_dict['current_Temperature'] = json_object['currently']['temperature']
+    new_dict['current_precipProbability'] = json_object['currently']['precipProbability']
+    new_dict['current_summary'] = json_object['currently']['summary']
+    new_dict['CURRENT_TIME'] = datetime.now()
+    return new_dict
+
+def pull_daily_data(json_object):
+    daily_dict = {}
+    daily_dict['daily_windSpeed'] = json_object['daily']['data'][0]['windSpeed']
+    daily_dict['daily_humidity'] = json_object['daily']['data'][0]['humidity']
+    daily_dict['daily_apparentTemperatureHigh'] = json_object['daily']['data'][0]['apparentTemperatureHigh']
+    daily_dict['daily_apparentTemperatureLow'] = json_object['daily']['data'][0]['apparentTemperatureLow']
+    daily_dict['daily_TemperatureLow'] = json_object['daily']['data'][0]['temperatureLow']
+    daily_dict['daily_TemperatureHigh'] = json_object['daily']['data'][0]['temperatureHigh']
+    daily_dict['daily_precipType'] = json_object['daily']['data'][0]['precipType']
+    daily_dict['daily_precipProbability'] = json_object['daily']['data'][0]['precipProbability']
+    daily_dict['daily_sunriseTime'] = json_object['daily']['data'][0]['sunriseTime']
+    daily_dict['daily_sunsetTime'] = json_object['daily']['data'][0]['sunsetTime']
+    daily_dict['CURRENT_TIME'] = datetime.now()
+    return daily_dict
+
+
+def get_daily_dict():
+    search_query = Coordinate_Request.run()
+    result = pull_daily_data(get_result(build_search_url(search_query[0], search_query[1])))
+    return result
+
+
+def get_current_dict():
+    search_query = Coordinate_Request.run()
+    result = pull_current_data(get_result(build_search_url(search_query[0], search_query[1])))
+    return result
+
+if __name__ == "__main__":
+    get_daily_dict()
+    get_current_dict()

017525adf2aca949219fc01bdd5deac8decc55f2
Lily Pham <lilypham@dhcp-v106-206.mobile.uci.edu>
2019-02-16 22:39:03 -0800

    Redirect submission to index

--- weatherApp/weather/urls.py
+++ weatherApp/weather/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = "weather"
 urlpatterns = [
     path('', views.index, name='index'),
-    path('comfortAsk/', views.comfortAsk, name='comfortAsk')
+    path('comfortAsk/', views.comfortAsk, name='comfortAsk'),
+    path('submission/', views.submission, name='submission')
     # ex: /polls/5/
     # path('<int:question_id>/', views.detail, name='detail'),
     # # ex: /polls/5/vote/
